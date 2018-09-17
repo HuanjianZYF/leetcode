f591e2a36edd737e5f99df4fcd115664694280ad
zhuangyuanfeng <zhuangyuanfeng@qbb6.com>
2018-09-17 20:02:36 +0800

    电话字母组合

--- src/one_twenty/LetterCombinationsOfPhoneNumber17.java
+++ src/one_twenty/LetterCombinationsOfPhoneNumber17.java
@@ -1,9 +1,6 @@
 package one_twenty;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author zyf
@@ -13,28 +10,47 @@ public class LetterCombinationsOfPhoneNumber17 {
 
 	public List<String> letterCombinations(String digits) {
 		//先初始化每个数字可以代表的字母
-		Map<Integer, String> digitLetterMap = new HashMap<>();
+		Map<Character, String> digitLetterMap = new HashMap<>();
 		initDigitLetterMap(digitLetterMap);
 		
-		List<String> result = new ArrayList<>();
+		Set<String> result = new HashSet<>();
 		if (digits == null || digits.length() == 0) {
-			return result;
+			return new ArrayList<>(result);
 		}
-		for (int i = 0; i < digits.length(); i++) {
-			
+		
+		for (int i = digits.length() - 1; i >= 0; i--) {
+			result = getResultSet(digits.charAt(i), result, digitLetterMap);
+		}
+		return new ArrayList<>(result);
+	}
+	
+	private Set<String> getResultSet(char c, Set<String> set, Map<Character, String> map) {
+		char[] chs = map.get(c).toCharArray();
+		if (set.size() == 0) {
+			for (char each : chs) {
+				set.add("" + each);
+			}
+			return set;
+		}
+		
+		Set<String> result = new HashSet<>();
+		for (String each : set) {
+			for (char character : chs) {
+				result.add(character + each);
+			}
 		}
 		return result;
 	}
 	
-	private void initDigitLetterMap(Map<Integer, String> map) {
-		map.put(2, "abc");
-		map.put(3, "def");
-		map.put(4, "ghi");
-		map.put(5, "jkl");
-		map.put(6, "mno");
-		map.put(7, "pqrs");
-		map.put(8, "tuv");
-		map.put(9, "wxyz");
+	private void initDigitLetterMap(Map<Character, String> map) {
+		map.put('2', "abc");
+		map.put('3', "def");
+		map.put('4', "ghi");
+		map.put('5', "jkl");
+		map.put('6', "mno");
+		map.put('7', "pqrs");
+		map.put('8', "tuv");
+		map.put('9', "wxyz");
 	}
 	
 }
---
+++ src/one_twenty/MediumOfTwoSortedArray4.java
@@ -0,0 +1,12 @@
+package one_twenty;
+
+/**
+ * @author zyf
+ * @date 2018/9/17 下午7:55
+ */
+public class MediumOfTwoSortedArray4 {
+	
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		return 0.0;
+	}
+}
