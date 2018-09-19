9d75a85c67d288e8ae53653dd20176aa44a08cd2
zhuangyuanfeng <zhuangyuanfeng@qbb6.com>
2018-09-19 20:00:07 +0800

    hah

---
+++ src/one_twenty/FourSum18.java
@@ -0,0 +1,18 @@
+package one_twenty;
+
+import java.util.List;
+
+/**
+ * @author zyf
+ * @date 2018/9/19 下午12:58
+ */
+public class FourSum18 {
+	public static void main(String[] args) {
+		new FourSum18().fourSum(null, 0);
+	}
+	
+	public List<List<Integer>> fourSum(int[] nums, int target) {
+		System.out.println("a" + null);
+		return null;
+	}
+}
