ab1c3816ad5f35888f669dee5c617dfd8c2a5a47
zhuangyuanfeng <zhuangyuanfeng@qbb6.com>
2018-09-20 11:46:04 +0800

    closest 3 sum

--- src/one_twenty/FourSum18.java
+++ src/one_twenty/FourSum18.java
@@ -1,5 +1,7 @@
 package one_twenty;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -8,11 +10,93 @@ import java.util.List;
  */
 public class FourSum18 {
 	public static void main(String[] args) {
-		new FourSum18().fourSum(null, 0);
+		System.out.println(new FourSum18().fourSum(new int[]{1,0,-1,0,-2,2}, 0));
 	}
 	
 	public List<List<Integer>> fourSum(int[] nums, int target) {
-		System.out.println("a" + null);
-		return null;
+		List<List<Integer>> result = new ArrayList<>();
+
+		if (nums == null || nums.length < 4) {
+			return result;
+		}
+
+		//先排序
+		Arrays.sort(nums);
+		for (int i = 0; i <= nums.length - 4; i++) {
+			if (i == 0 || nums[i] != nums[i - 1]) {
+				int value = nums[i];
+				
+				List<List<Integer>> subList = findThreeSum(Arrays.copyOfRange(nums, i + 1, nums.length), target - value);
+				for (List<Integer> each : subList) {
+					each.add(value);
+					result.add(each);
+				}
+			}
+		}
+		
+		return result;
+	}
+
+
+	/**
+	 * 寻找数组中，三个数和为sum的组合
+	 * @param nums 待搜索的数组，需要从小到大排列，且数量是大于3的
+	 * @param sum 和
+	 * @return 返回的是list的list，里面的list是和为sum的加数
+	 */
+	private List<List<Integer>> findThreeSum(int[] nums, int sum) {
+		List<List<Integer>> result = new ArrayList<>();
+
+		for (int i = 0; i <= nums.length - 3; i++) {
+			if (i == 0 || nums[i] != nums[i - 1]) {
+				int value = nums[i];
+
+				List<List<Integer>> subList = findTwoSum(Arrays.copyOfRange(nums, i + 1, nums.length), sum - value);
+				for (List<Integer> each : subList) {
+					each.add(value);
+					result.add(each);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * 寻找数组中有没有两个数的和为sum
+	 * @param nums 待搜索的数组，需要是从小到大排列的
+	 * @param sum 和
+	 * @return 返回的是list的list，里面的list是和为sum的加数
+	 */
+	private List<List<Integer>> findTwoSum(int[] nums, int sum) {
+		List<List<Integer>> result = new ArrayList<>();
+
+		for (int i = 0; i <= nums.length - 2; i++) {
+			if (i == 0 || nums[i] != nums[i - 1]) {
+				int value = nums[i];
+				Integer a = searchOne(nums, i, sum - value);
+				if (a != null) {
+					List<Integer> list = new ArrayList<>();
+					list.add(value);
+					list.add(a);
+					result.add(list);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	/**
+	 * 寻找数组中有没有一个数等于target，从i开始寻找
+	 * @param nums 搜索的数组
+	 * @param i 起始下标
+	 * @param target 目标值
+	 * @return 如果找到了，返回target，找不到则返回null
+	 */ 
+	private Integer searchOne(int[] nums, int i, int target) {
+		int index = Arrays.binarySearch(nums, i + 1, nums.length, target);
+
+		return index >= 0 ? target : null;
 	}
 }
---
+++ src/one_twenty/ThreeSumClosest16.java
@@ -0,0 +1,93 @@
+package one_twenty;
+
+import java.util.Arrays;
+
+/**
+ * @author zyf
+ * @date 2018/9/20 上午10:12
+ */
+public class ThreeSumClosest16 {
+	
+	public static void main(String[] args) {
+		System.out.println(new ThreeSumClosest16().threeSumClosest(new int[]{-4,-1,1,2},-10));
+	}
+	
+	public int threeSumClosest(int[] nums, int target) {
+		int result = Integer.MAX_VALUE;
+		int diff = Integer.MAX_VALUE;
+		
+		if (nums == null || nums.length < 3) {
+			return result;
+		}
+
+		//将问题归约至求两数和最小的那个
+		Arrays.sort(nums);
+		for (int i = 0; i <= nums.length - 3; i ++) {
+			if (i == 0 || nums[i] != nums[i - 1]) {
+				int subResult = findTwoSumClosest(Arrays.copyOfRange(nums, i + 1, nums.length), target - nums[i]);
+				int nowDiff = Math.abs(target - subResult - nums[i]);
+				if (nowDiff < diff) {
+					diff = nowDiff;
+					result  = subResult + nums[i];
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	private int findTwoSumClosest(int[] nums, int sum) {
+		int result = Integer.MAX_VALUE;
+		int diff = Integer.MAX_VALUE;
+		
+		for (int i = 0; i <= nums.length - 2; i++) {
+			if (i == 0 || nums[i] != nums[i - 1]) {
+				int subResult = findOneClosest(nums, i + 1, sum - nums[i]);
+				int nowDiff = Math.abs(sum - subResult - nums[i]);
+				if (nowDiff < diff) {
+					diff = nowDiff;
+					result = subResult + nums[i];
+				}
+			}
+		}
+		
+		return result;
+	}
+	
+	private int findOneClosest(int[] nums, int start, int target) {
+		int diff = Integer.MAX_VALUE;
+		boolean flag = false; //表示比target大还是小 false表示小
+		
+		int end = nums.length;
+		if (nums[start] >= target) {
+			return nums[start];
+		}
+		if (nums[end - 1] <= target) {
+			return nums[end - 1];
+		}
+		
+		while (start < end) {
+			int mid = (start + end) / 2;
+			if (nums[mid] == target) {
+				return target;
+			} else if (nums[mid] < target) {
+				int nowDiff = target - nums[mid];
+				if (nowDiff < diff) {
+					diff = nowDiff;
+					flag = false;
+				}
+				start = mid + 1;
+			} else {
+				int nowDiff = nums[mid] - target;
+				if (nowDiff < diff) {
+					diff = nowDiff;
+					flag = true;
+				}
+				end = mid - 1;
+			}
+		}
+		
+		return flag ? target + diff : target - diff;
+	}
+	
+}
